540c02b3509b45ed6f8c403bb462911d9468772b
Antonis Geralis <b3lievem@yandex.com>
2023-06-03 11:32:24 +0300

    add tests

--- tests/tcowstrings.nim
+++ tests/tcowstrings.nim
@@ -1,5 +1,5 @@
 import cowstrings
-import std/[isolation, assertions]
+import std/[enumerate, isolation, assertions]
 
 proc main =
   block:
@@ -29,5 +29,28 @@ proc main =
     prepareMutation(b)
     b[0] = 'P'
     assert a == cstring"World".toStr
+  block:
+    let data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var str = String()
+    for c in data:
+      str.add c
+    let expected = toStr(data)
+    assert str.len == data.len
+    assert str == expected
+  block:
+    let data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var a: seq[String] = @[]
+    for i in 0..data.len:
+      let strLen = data.len - i
+      let expected = data[0..<strLen]
+      var str = toStr(expected)
+      a.add(str)
+      assert str.toCStr == expected.cstring
+      assert str.len == strLen
+    for i, str in enumerate(items(a)):
+      let strLen = data.len - i
+      let expected = data[0..<strLen]
+      assert str.toCStr == expected.cstring
+      assert str.len == strLen
 
 main()
